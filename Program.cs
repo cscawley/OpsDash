37391a4d873c4c52829dc00514b4886a59303d94
cscawley <cscawley@mac.com>
2022-05-22 16:06:46 -0500

    add Identity packages, add identity services to startup, refactor models directory dividing the two concearns for readability.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TrivyDash.Data;
+using TrivyDash.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 if (builder.Environment.IsProduction())
@@ -14,17 +16,23 @@ if (builder.Environment.IsProduction())
     Console.WriteLine("Using InMem DbContext");
     builder.Services.AddDbContext<AppDbContext>(opt =>
             opt.UseInMemoryDatabase("InMem"));
+    
 }
 
+builder.Services.AddDefaultIdentity<AppDbContext>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
 
+builder.Services.AddIdentityServer()
+    .AddApiAuthorization<ApplicationUser, AppDbContext>();
+
+builder.Services.AddAuthentication()
+    .AddIdentityServerJwt();
+
+builder.Services.AddAuthorization();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IReportRepo, ReportRepo>();
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer();
-builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Swagger/OpenAPI https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -39,15 +47,17 @@ if (!app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseMigrationsEndPoint();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseIdentityServer();
+app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
